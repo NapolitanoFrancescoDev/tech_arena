ce128c81e54398152d557a12f1e04380a4323a44
Napolitanof <francescocoding99@gmail.com>
2025-01-15 16:56:09 +0100

    Creazione create e delete

--- back-end/app/Http/Controllers/Admin/ColorController.php
+++ back-end/app/Http/Controllers/Admin/ColorController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Models\Color;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AddColorRequest;
+use App\Http\Requests\UpdateColorRequest;
 
 class ColorController extends Controller
 {
@@ -13,7 +15,10 @@ class ColorController extends Controller
      */
     public function index()
     {
-        
+        return view('admin.colors.index')->with([
+            'colors' => Color::latest()->get()
+
+        ]);
     }
 
     /**
@@ -21,15 +26,22 @@ class ColorController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.colors.create');
     }
+    
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(AddColorRequest $request)
     {
-        //
+        if($request->validated())
+        {
+            Color::create($request->validated());
+            return redirect()->route('admin.colors.index')->with([
+                'succes' => 'Color has been added seccessfully'
+            ]);
+        }
     }
 
     /**
@@ -37,7 +49,7 @@ class ColorController extends Controller
      */
     public function show(Color $color)
     {
-        //
+        abort(404);
     }
 
     /**
@@ -45,15 +57,24 @@ class ColorController extends Controller
      */
     public function edit(Color $color)
     {
-        //
+        return view('admin.colors.edit')->with([
+                'color' => $color
+     ]);
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Color $color)
+    public function update(UpdateColorRequest $request, Color $color)
     {
-        //
+        if($request->validated())
+        {
+           $color->update($request->validated());
+            return redirect()->route('admin.colors.index')->with([
+                'succes' => 'Color has been updated seccessfully'
+            ]);
+        }
     }
 
     /**
@@ -61,6 +82,9 @@ class ColorController extends Controller
      */
     public function destroy(Color $color)
     {
-        //
+        $color->delete();
+        return redirect()->route('admin.colors.index')->with([
+            'succes' => 'Color has been deleted seccessfully'
+        ]);
     }
 }
--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -2,15 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\ColorController;
 
 Route::get('/',[AdminController::class, 'login'])->name('admin.login');
 Route::post('/admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
 
 
 Route::middleware('admin')->group(function(){
+    Route::prefix('admin')->group(function(){
+        Route::get('dashboard', [AdminController::class, 'index'])->name('admin.index');
+        Route::post('logout', [AdminController::class, 'logout'])->name('admin.logout');
+        //colors routes
+        Route::resource('colors', ColorController::class)->names([
+            'index' => 'admin.colors.index',
+            'create' => 'admin.colors.create',
+            'store' => 'admin.colors.store',
+            'edit' => 'admin.colors.edit',
+            'update' => 'admin.colors.update',
+            'destroy' => 'admin.colors.destroy',
+        ]);
 
-    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
-    Route::post('/admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
-
+    });
+    
+    
 
 });
